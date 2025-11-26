5ea1b6fafaf8fa1f1a7797a5efc7ab8da8254340
hxf0223 <hxf0223@hotmail.com>
2025-11-26 08:23:56 +0800

    fix run swarm plane bash script

--- run_swarm_2_plane.sh
+++ run_swarm_2_plane.sh
@@ -2,7 +2,7 @@
 
 uv sync
 uv run ap-sitl-swarm --model plane -n 2\
-    --data-dir ~/tmp/arduplane \
+    --data-dir ~/tmp/ardu \
     --tcp-base-port 5760 \
     --home 31.8269,117.2280,30\
-    ~/tmp/arduplane/arduplane
+    ~/tmp/ardu/arduplane
